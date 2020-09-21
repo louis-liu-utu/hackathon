b4564d37adafe72db98ddf7f7f507ca4c213e60e
liuyingping <lypk21@gmail.com>
2020-09-21 21:09:42 +1000

    fix video

--- resources/js/index.js
+++ resources/js/index.js
@@ -11,18 +11,21 @@ if (supportsVideo) {
 
     let headerDisplay = "none"
     if(home1.offsetHeight < home1.offsetWidth)
-        homeVideo.style.height =  home1.offsetHeight + "px";
+        homeVideo.style.height =  (home1.offsetHeight - 5) + "px";
     else {
         homeVideo.style.width = home1.offsetWidth + "px";
-        homeVideo.style.height = home1.offsetHeight + "px";
+        homeVideo.style.height = (home1.offsetHeight - 5) + "px";
         headerDisplay = "block"
     }
+
+    console.log('home1',home1.offsetHeight);
 
 
     iconPlayer.addEventListener('click', function (e) {
         home1.style.display = "none";
         header.style.display = headerDisplay;
         home0.style.display = "block";
+        console.log('home0',home0.offsetHeight);
 
         if (homeVideo.paused || homeVideo.ended) homeVideo.play();
         else homeVideo.pause();
